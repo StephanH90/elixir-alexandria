e2a2da7cfc0f159d18e33f6f8eea6b4617d2ea5f
Stephan Hug <stephan.hug@adfinis.com>
2026-05-15 16:14:24 +0200

    feat: load categories and documents

--- dev/lib/alexandria_dev_web/browser_live.ex
+++ dev/lib/alexandria_dev_web/browser_live.ex
@@ -29,7 +29,7 @@ defmodule AlexandriaDevWeb.BrowserLive do
 
             <div class="uk-padding-small uk-height-1-1 uk-overflow-auto document-grid--disallowed uk-border-top">
               <div class="document-view">
-                <.document_list />
+                <.document_list documents={@documents}/>
               </div>
               <!---->
             </div>
@@ -47,11 +47,35 @@ defmodule AlexandriaDevWeb.BrowserLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    scope = %AlexandriaDev.Scope{locale: "de-ch"}
+    scope = %AlexandriaDev.Scope{locale: "de-ch", actor: %{role: :support}}
     categories = Alexandria.Core.list_root_categories!(scope: scope, load: :active_document_count)
 
-    socket = assign(socket, categories: categories, active_category: List.first(categories))
+    socket = assign(socket, categories: categories, active_category: get_active_category(categories), scope: scope)
 
     {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(%{"category" => category}, _session, socket) do
+    # todo fetch documents
+    active_category = get_active_category(socket.assigns.categories, category)
+    documents = Alexandria.Core.list_active_documents_by_category!(active_category.slug, scope: socket.assigns.scope, load: :category)
+
+    socket = assign(socket, documents: documents, active_category: active_category)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_params(_params, _session, socket), do: {:noreply, socket}
+
+  @impl true
+  def handle_event("alexandria:navigate", %{"category" => category}, socket) do
+    {:noreply, push_patch(socket, to: ~p"/?#{[category: category]}")}
+  end
+
+  defp get_active_category([first_category | _rest]), do: first_category
+  defp get_active_category(categories, category_slug) do
+    Enum.find(categories, fn cat -> cat.slug == category_slug end)
   end
 end
