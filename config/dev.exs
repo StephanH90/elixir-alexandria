970c279bda1d350fd3343e1318f2ca5084ab0abe
Stephan Hug <stephan.hug@adfinis.com>
2026-05-24 10:04:30 +0200

    fix(dev): enable automatic reload of alexandria in dev

--- config/dev.exs
+++ config/dev.exs
@@ -9,3 +9,11 @@ config :alexandria, Alexandria.Repo,
   pool_size: 10
 
 config :ash, policies: [show_policy_breakdowns?: true]
+
+config :alexandria, :storage,
+  adapter: AlexandriaDev.Storage.ExAws,
+  bucket: System.get_env("ALEXANDRIA_S3_BUCKET", "alexandria-media"),
+  access_key_id: System.get_env("ALEXANDRIA_S3_ACCESS_KEY_ID", "dev"),
+  secret_access_key: System.get_env("ALEXANDRIA_S3_SECRET_ACCESS_KEY", "dev"),
+  endpoint_url: System.get_env("ALEXANDRIA_S3_ENDPOINT_URL", "http://localhost:3900"),
+  region: "garage"
--- dev/config/dev.exs
+++ dev/config/dev.exs
@@ -3,6 +3,7 @@ import Config
 config :alexandria_dev, AlexandriaDevWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
+  reloadable_apps: [:alexandria_dev, :alexandria],
   check_origin: false,
   watchers: [
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
@@ -18,10 +19,4 @@ config :alexandria_dev, AlexandriaDevWeb.Endpoint,
     ]
   ]
 
-config :alexandria, :storage,
-  adapter: AlexandriaDev.Storage.ExAws,
-  bucket: System.get_env("ALEXANDRIA_S3_BUCKET", "alexandria-media"),
-  access_key_id: System.get_env("ALEXANDRIA_S3_ACCESS_KEY_ID", "dev"),
-  secret_access_key: System.get_env("ALEXANDRIA_S3_SECRET_ACCESS_KEY", "dev"),
-  endpoint_url: System.get_env("ALEXANDRIA_S3_ENDPOINT_URL", "http://localhost:3900"),
-  region: "garage"
+config :phoenix_live_reload, :dirs, [".", ".."]
